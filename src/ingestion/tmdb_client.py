7e51cfb151098f7bc516b2283ba1df5464159413
Gabriel Sousa <ghostamino08@gmail.com>
2026-04-20 22:46:25 -0300

    logging and timestamp raw data ingestion

--- src/ingestion/tmdb_client.py
+++ src/ingestion/tmdb_client.py
@@ -2,8 +2,11 @@ import requests
 import time
 import json
 import os
+import logging
+from datetime import datetime
 from pathlib import Path
 from dotenv import load_dotenv
+from src.utils.logger import setup_logger
 
 env_path = Path(__file__).resolve().parents[2] / ".env"
 load_dotenv(env_path)
@@ -13,6 +16,8 @@ BASE_URL = os.getenv("BASE_URL")
 
 OUTPUT_PATH = Path("data/raw/movies_raw.json")
 
+setup_logger()
+logger = logging.getLogger(__name__)
 
 def fetch_movies(page: int):
     url = f"{BASE_URL}/movie/popular"
@@ -34,29 +39,35 @@ def ingest_movies(max_pages=5):
     all_movies = []
 
     for page in range(1, max_pages + 1):
-        print(f"Coletando página {page}...")
+        logger.info(f"Coletando página {page}")
 
-        data = fetch_movies(page)
-        results = data.get("results", [])
+        try:
+            data = fetch_movies(page)
+            results = data.get("results", [])
 
-        all_movies.extend(results)
+            all_movies.extend(results)
+
+        except Exception as e:
+            logger.error(f"Erro ao coletar página {page}: {e}")
 
         time.sleep(0.3)  # evitar rate limit
 
+    logger.info(f"Total de filmes coletados: {len(all_movies)}")
+    
     return all_movies
 
 
 def save_raw(data):
-    OUTPUT_PATH.parent.mkdir(parents=True, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y-%m-%d")
+    output_file = Path(f"data/raw/movies_raw_{timestamp}.json")
+    output_file.parent.mkdir(parents=True, exist_ok=True)
 
-    with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
-    print("API_KEY:", API_KEY)
-    print("BASE_URL:", BASE_URL)
     movies = ingest_movies(max_pages=10)
     save_raw(movies)
 
-    print(f"{len(movies)} filmes salvos em RAW.")
+    logger.info(f"Quantidade de filmes salvos em '{OUTPUT_PATH}': {len(movies)} filmes.")
